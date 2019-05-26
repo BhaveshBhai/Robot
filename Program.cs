2600a238ae77e3b197330e5f4423f64685ff77a7
BhaveshBhai <osi.bhavesh@gmail.com>
2019-05-26 23:56:13 +1000

    Add Code
    New code added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ToyRobotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           RobotSimulator firstTestCase = new RobotSimulator("PLACE 0,0,NORTH MOVE REPORT");
+           firstTestCase.RunRobot();
+
+           RobotSimulator secondTestCase = new RobotSimulator("PLACE 0,0,NORTH LEFT REPORT");
+           secondTestCase.RunRobot();
+
+           RobotSimulator thirdTestCase = new RobotSimulator("PLACE 1,2,EAST MOVE MOVE LEFT MOVE REPORT");
+           thirdTestCase.RunRobot();
+
+           RobotSimulator invalidTestCase = new RobotSimulator("MOVE REPORT");
+           invalidTestCase.RunRobot();
+
+           Console.ReadLine();
+        }
+    }
+}
